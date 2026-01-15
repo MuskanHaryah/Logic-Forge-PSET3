71210ac15724cfda1d25c391f07fb9cc7f55ee5a
MuskanHaryah <muskanharyah36@gmail.com>
2026-01-15 10:55:11 +0500

    Optimized solution for challenge 6

--- challenge_06_suspicious_device_detection.py
+++ challenge_06_suspicious_device_detection.py
@@ -1,11 +1,25 @@
+# def find_repeated_identifier(nums):
+#     n = len(nums) // 2
+#     for x in nums:
+#         count = 0
+#         for y in nums:
+#             if x == y:
+#                 count += 1
+#         if count == n:
+#             return x
+
+
+# nums = [2, 1, 2, 5, 3, 2]
+# print(find_repeated_identifier(nums))
+
+
 def find_repeated_identifier(nums):
-    n = len(nums) // 2
+    freq = {}
+    target = len(nums) // 2
+
     for x in nums:
-        count = 0
-        for y in nums:
-            if x == y:
-                count += 1
-        if count == n:
+        freq[x] = freq.get(x, 0) + 1
+        if freq[x] == target:
             return x
 
 
